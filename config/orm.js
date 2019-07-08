876998057a2bc675b70c01fa3315c66f99f7f0be
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-07 21:49:34 -0700

    Adding objToSql function within orm.js

--- config/orm.js
+++ config/orm.js
@@ -8,3 +8,19 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
+function objToSql(object) {
+    var arr = [];
+
+    for (var key in object) {
+        var value = object[key];
+        if (Object.hasOwnProperty.call(object, key)) {
+            if (typeof vallue === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
