807ea5470cca4c8838c8457a64ca064f5c5408bb
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-08 18:04:53 -0700

    Adding an update route within burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,8 +15,25 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burgers.create(["burger_name", "devoured"], [req.body.burger_name], function(result) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name], function(result) {
         res.json({ id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     });
 });
 
